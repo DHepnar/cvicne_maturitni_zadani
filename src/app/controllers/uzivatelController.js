af63a54bc59dd6cc97a8fe6a528a12cf4842e53b
Dominik Hepnar <hepnar@spstrutnov.cz>
2022-04-03 15:21:35 +0200

    Registrační formulář

--- src/app/controllers/uzivatelController.js
+++ src/app/controllers/uzivatelController.js
@@ -1,6 +1,11 @@
+const { render } = require("..");
+
 exports.zobrazPrihlasit_page = (dotaz, odpoved) => {
     odpoved.render('uzivatel/prihlasit_page'); 
 };
 exports.zobrazRegistrovat_page = (dotaz, odpoved) => {
     odpoved.render('uzivatel/registrovat_page'); 
 };
+exports.registrovat = (dotaz, odpoved) => {
+    odpoved
+}
--- src/app/routers/uzivatelRouter.js
+++ src/app/routers/uzivatelRouter.js
@@ -8,5 +8,6 @@ const controller = require(path.join(__dirname, '..', 'controllers', 'uzivatelCo
 
 router.get('/prihlasit_page', controller.zobrazPrihlasit_page);
 router.get('/registrovat_page', controller.zobrazRegistrovat_page);
+router.post('/registrovat', controller.registrovat);
 
 module.exports = router;
